77f6841678cea351faaedc47391ecd7571dbe940
Jason <jasongoesplaces@gmail.com>
2018-07-21 10:59:47 -0400

    fixed critical errors pt.3

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/explore");
-// const seeds = require("./scripts/guideSeeds")
+// const seeds = require("./scripts/guideSeeds") 
 
 // Start the API server
 app.listen(PORT, function() {
