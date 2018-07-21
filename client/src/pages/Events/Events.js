df826ec7e6c3c7fe82227860fd55faf2a6964007
Jason <jasongoesplaces@gmail.com>
2018-07-21 09:53:20 -0400

    security fix and page added

--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -8,17 +8,19 @@ class Events extends Component {
             <NavBar/>
             <div className="row">
                 <div className="col m4 hide-on-small-only"></div>
-                <div className="col s12 m4">
-                    <form className="input-field searchField">
+                <div className="col s12 m4 searchField">
+                    <form className="input-field seachFieldBG">
                         <div className="row">
+                            <div className="col s1"></div>
                             <div className="col s8">
-                                <i className="material-icons prefix">search</i>
+                                <i className="material-icons prefix eventSearchIcon">search</i>
                                 <input id="icon_prefix" type="text" className="searchBar eventSearch"/>
                                 <label className="searchLabel" htmlFor="icon_prefix">Enter a City to find Events</label>
                             </div>
-                            <div className="col s4">
+                            <div className="col s2">
                                 <button id="eventSearchBtn" href="#" className="eventButton">Search</button>
                             </div>
+                            <div className="col s1"></div>
                         </div>
                     </form>
                 </div>
