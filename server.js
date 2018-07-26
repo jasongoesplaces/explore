2e6da2cf9f6d664700c7ab35c717ff903e56224d
Jason <jasongoesplaces@gmail.com>
2018-07-26 15:49:57 -0400

    minor server fix

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/auth', jwtMW, (req, res) => {
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
